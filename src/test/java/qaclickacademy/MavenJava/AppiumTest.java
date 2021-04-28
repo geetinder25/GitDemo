1d803065fb426a1f5322b419441110a0f77fac33
Geetinder <dhillon.geetu@gmail.com>
2021-04-28 16:08:52 +0530

    first commit

---
+++ src/test/java/qaclickacademy/MavenJava/AppiumTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void nativeAppAndroid() {
+		System.out.println("nativeAppAndroid");
+	}
+	
+	@Test
+	public void iOSApp() {
+		System.out.println("iOSApp");
+	}
+}
